5271941cc15a371e710df780c2e4528eee62865c
IceCruelStuff <50642756+IceCruelStuff@users.noreply.github.com>
2020-05-04 18:08:45 -0700

    Update Main.php

--- src/Inaayat/CPSTag/Main.php
+++ src/Inaayat/CPSTag/Main.php
@@ -8,57 +8,58 @@ use pocketmine\plugin\PluginBase;
 use pocketmine\event\Listener;
 use pocketmine\Player;
 use pocketmine\event\server\DataPacketReceiveEvent;
-use pocketmine\network\mcpe\protocol\InventoryTransactionPacket;
+use pocketmine\network\protocol\InventoryTransactionPacket;
 use pocketmine\utils\Config;
 use Inaayat\CPSTag\CPSTagTask;
 
 class Main extends PluginBase implements Listener {
 
-	private $clicks;
-	public $config;
+    private $clicks;
+    public $config;
 	
-	public function onEnable(){
-		@mkdir($this->getDataFolder());
-		$this->saveDefaultConfig();
-		$this->getServer()->getPluginManager()->registerEvents($this ,$this);
-		$this->getScheduler()->scheduleRepeatingTask(new CPSTagTask($this), 10);
-	}
+    public function onEnable(){
+        @mkdir($this->getDataFolder());
+        $this->saveDefaultConfig();
+        $this->getServer()->getPluginManager()->registerEvents($this ,$this);
+        $this->getScheduler()->scheduleRepeatingTask(new CPSTagTask($this), 10);
+    }
 
-	public function getCPS(Player $player): int{
-		if(!isset($this->clicks[$player->getLowerCaseName()])){
-			return 0;
-		}
-		$time = $this->clicks[$player->getLowerCaseName()][0];
-		$clicks = $this->clicks[$player->getLowerCaseName()][1];
-		if($time !== time()){
-			unset($this->clicks[$player->getLowerCaseName()]);
-			return 0;
-		}
-		return $clicks;
-	}
+    public function getCPS(Player $player): int{
+        if(!isset($this->clicks[$player->getLowerCaseName()])){
+            return 0;
+        }
+        $time = $this->clicks[$player->getLowerCaseName()][0];
+        $clicks = $this->clicks[$player->getLowerCaseName()][1];
+        if($time !== time()){
+            unset($this->clicks[$player->getLowerCaseName()]);
+            return 0;
+        }
+        return $clicks;
+    }
 	
-	public function addCPS(Player $player): void{
-		if(!isset($this->clicks[$player->getLowerCaseName()])){
-			$this->clicks[$player->getLowerCaseName()] = [time(), 0];
-		}
-		$time = $this->clicks[$player->getLowerCaseName()][0];
-		$clicks = $this->clicks[$player->getLowerCaseName()][1];
-		if($time !== time()){
-			$time = time();
-			$clicks = 0;
-		}
-		$clicks++;
-		$this->clicks[$player->getLowerCaseName()] = [$time, $clicks];
-	}
+    public function addCPS(Player $player): void{
+        if(!isset($this->clicks[$player->getLowerCaseName()])){
+            $this->clicks[$player->getLowerCaseName()] = [time(), 0];
+        }
+        $time = $this->clicks[$player->getLowerCaseName()][0];
+        $clicks = $this->clicks[$player->getLowerCaseName()][1];
+        if($time !== time()){
+            $time = time();
+            $clicks = 0;
+        }
+        $clicks++;
+        $this->clicks[$player->getLowerCaseName()] = [$time, $clicks];
+    }
 
-	public function onDataPacketReceive(DataPacketReceiveEvent $event){
-		$player = $event->getPlayer();
-		$packet = $event->getPacket();
-		if($packet instanceof InventoryTransactionPacket){
-			$transactionType = $packet->transactionType;
-			if($transactionType === InventoryTransactionPacket::TYPE_USE_ITEM || $transactionType === InventoryTransactionPacket::TYPE_USE_ITEM_ON_ENTITY){
-				$this->addCPS($player);
-			}
-		}
-	}
+    public function onDataPacketReceive(DataPacketReceiveEvent $event){
+        $player = $event->getPlayer();
+        $packet = $event->getPacket();
+        if($packet instanceof InventoryTransactionPacket){
+            $transactionType = $packet->transactionType;
+            if($transactionType === InventoryTransactionPacket::TYPE_USE_ITEM || $transactionType === InventoryTransactionPacket::TYPE_USE_ITEM_ON_ENTITY){
+                $this->addCPS($player);
+            }
+        }
+    }
+
 }
